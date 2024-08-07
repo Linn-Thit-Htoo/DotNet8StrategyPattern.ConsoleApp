14fe6a8818748bc0795b31533ff9075a3a4cb5b3
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-07 11:41:29 +0630

    -

--- DotNet8StrategyPattern.ConsoleApp/Program.cs
+++ DotNet8StrategyPattern.ConsoleApp/Program.cs
@@ -1,54 +1,57 @@
-﻿public class Program
+﻿namespace DotNet8StrategyPattern.ConsoleApp
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        Context context;
+        public static void Main(string[] args)
+        {
+            Context context;
 
-        context = new Context(new ConcreteStrategyA());
-        context.ExecuteStrategy();
+            context = new Context(new ConcreteStrategyA());
+            context.ExecuteStrategy();
 
-        context = new Context(new ConcreteStrategyB());
-        context.ExecuteStrategy();
-    }
-}
-
-public interface IStrategy
-{
-    void Execute();
-}
-
-public class ConcreteStrategyA : IStrategy
-{
-    public void Execute()
-    {
-        Console.WriteLine("Strategy A executed.");
-    }
-}
-
-public class ConcreteStrategyB : IStrategy
-{
-    public void Execute()
-    {
-        Console.WriteLine("Strategy B executed.");
-    }
-}
-
-public class Context
-{
-    private IStrategy _strategy;
-
-    public Context(IStrategy strategy)
-    {
-        _strategy = strategy;
+            context = new Context(new ConcreteStrategyB());
+            context.ExecuteStrategy();
+        }
     }
 
-    public void SetStrategy(IStrategy strategy)
+    public interface IStrategy
     {
-        _strategy = strategy;
+        void Execute();
     }
 
-    public void ExecuteStrategy()
+    public class ConcreteStrategyA : IStrategy
     {
-        _strategy.Execute();
+        public void Execute()
+        {
+            Console.WriteLine("Strategy A executed.");
+        }
+    }
+
+    public class ConcreteStrategyB : IStrategy
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Strategy B executed.");
+        }
+    }
+
+    public class Context
+    {
+        private IStrategy _strategy;
+
+        public Context(IStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public void SetStrategy(IStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public void ExecuteStrategy()
+        {
+            _strategy.Execute();
+        }
     }
 }
